7ea3917e85886b0acb42ee5bb166d38b0794f539
Richard H <richard@haddads.net.au>
2022-10-08 17:54:06 +1000

    config: fix NavBar on home page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Head } from '@inertiajs/inertia-react';
+import NavBar from "@/Components/NavBar";
 
 export default function Welcome(props) {
     return (
