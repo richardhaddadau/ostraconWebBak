0066211dfcda9fa5b0ec564a09230701ac5454c4
Richard Haddad <richard@haddads.net.au>
2022-10-09 06:48:31 +1000

    config: install sass

--- resources/js/Components/SideNav.jsx
+++ resources/js/Components/SideNav.jsx
@@ -3,10 +3,10 @@ import Navigation from "@/Components/Navigation";
 
 const NavBar = () => {
     return (
-        <>
+        <nav className="">
             <Navigation />
-        </>
-    )
-}
+        </nav>
+    );
+};
 
-export default NavBar
+export default NavBar;
